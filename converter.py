588bd6ea762f712ac5b394247358171408d8a2e5
Malik Jacobs <138883098+malikProgrammer1@users.noreply.github.com>
2023-08-06 00:52:42 -0400

    Update converter.py
    Code for the weight converter.

--- converter.py
+++ converter.py
@@ -1,7 +1,8 @@
+# Importing tkinter class
 from tkinter import *
 from tkinter import ttk
 
-
+# Functions for the "solve" buttons, which carry out the weight conversions.
 def solve(*args):
     try:
         value = float(ounces.get())
@@ -15,6 +16,7 @@ def solve2(*args):
     except ValueError:
         pass
 
+# Creating the textbox for the weight converter.
 root = Tk()
 root.title("Weight Converter")
 
@@ -23,6 +25,7 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+# Creation of the input units
 ounces = StringVar()
 ounces_entry = ttk.Entry(mainframe, width=7, textvariable=ounces)
 ounces_entry.grid(column=2, row=1, sticky=(W, E))
@@ -31,15 +34,18 @@ pounds = StringVar()
 pounds_entry = ttk.Entry(mainframe, width=7, textvariable=pounds)
 pounds_entry.grid(column=2, row=4, sticky=(W, E))
 
+# Creation of the output units
 grams = StringVar()
 ttk.Label(mainframe, textvariable=grams).grid(column=2, row=2, sticky=(W, E))
 
 ounces2 = StringVar()
 ttk.Label(mainframe, textvariable=ounces2).grid(column=2, row=5, sticky=(W, E))
 
+# Creation of the two solve buttons
 ttk.Button(mainframe, text="Calculate", command=solve).grid(column=3, row=3, sticky=W)
 ttk.Button(mainframe, text="Calculate", command=solve2).grid(column=3, row=6, sticky=W)
 
+# Text for completing the converter
 ttk.Label(mainframe, text="ounces").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, text="grams").grid(column=3, row=2, sticky=W)
@@ -47,6 +53,7 @@ ttk.Label(mainframe, text="pounds").grid(column=3, row=4, sticky=W)
 ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=5, sticky=E)
 ttk.Label(mainframe, text="ounces").grid(column=3, row=5, sticky=W)
 
+# Padding, binding, and other additonal touches
 for child in mainframe.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
 
